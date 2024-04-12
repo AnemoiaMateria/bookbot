1df8af2b28ded15ff67820aaf49c4dc6315c620c
AnemoiaMateria <khangtnguyen21@gmail.com>
2024-04-12 15:04:59 -0500

    updated to ignore text additions to git, main.py update to read text file and print to console

---
+++ main.py
@@ -0,0 +1,12 @@
+#main() is a function that takes the assigns the PATH, assigns function 'text' to read the PATH, and then prints 'text' to console.
+def main():
+    book_path = "books/Frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+#get_book_text() takes
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+main()
